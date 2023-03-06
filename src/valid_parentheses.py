1812cb8ce9f7927993519f7d8749be6a7bc902c6
tomasdiazwahl <tomasdiazwahl@gmail.com>
2023-03-05 17:45:25 -0800

    second commit

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -19,4 +19,32 @@ def is_match_parentheses_simple(s: str):
         return False
 
 
+def is_match_parentheses_simple_ii(s: str):
+    count = 0
+    for letter in s:
+        if letter == ')':
+            if count <= 0:
+                return False
+            else:
+                count -= 1
+        elif letter == '(':
+            count += 1
+        else:
+            return False
+    if count == 0:
+        return True
+    else:
+        return False
+
+
+def main():
+    stringinput = "(()))("
+
+    is_match_parentheses_simple_ii(stringinput)
+# end main
+
+
+if __name__ == "__main__":
+    main()
+
 
